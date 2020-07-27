e2080e5ebd86430cdaf76686b20caf1071b83682
alfz0r <alfredo.poesina@gmail.com>
2020-07-27 13:38:15 +0300

    added authentication

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Toolbar.module.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 //import ToggleButton from '../SideDrawer/ToggleButton/ToggleButton';
-import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <DrawerToggle clicked={props.drawerToggleClicked}/>
-        <div className={classes.Logo}>
-            <Logo />
-        </div>
-        
-        <nav className={classes.DesktopOnly}>
-            <NavigationItems />
-        </nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems isAuthenticated={props.isAuth} />
+    </nav>
+  </header>
 );
 
 export default toolbar;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,40 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import Aux from '../Auxiliary/Auxiliary';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';    //'../Navigation/Toolbar/Toolbar';
-import classes from './Layout.module.css';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';    //'../Navigation/SideDrawer/SideDrawer';
+import Aux from "../Auxiliary/Auxiliary";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar"; //'../Navigation/Toolbar/Toolbar';
+import classes from "./Layout.module.css";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer"; //'../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: false
-    }
-    
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    }
+  state = {
+    showSideDrawer: false,
+  };
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !this.state.showSideDrawer};
-        });
-    }
-    
-    // toggleButtonHandler = () => {
-    //     this.setState({showSideDrawer: true});
-    // }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    render () {
-        return (
-            <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Aux>
-        )
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !this.state.showSideDrawer };
+    });
+  };
+
+  // toggleButtonHandler = () => {
+  //     this.setState({showSideDrawer: true});
+  // }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+        />
+        <SideDrawer
+          isAuth={this.props.isAuthenticated}
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
 }
 
-export default Layout;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
